202329ae0b39358398582e7fbe525dca445de43e
Beks_jf39@student.itstep.kz <Beks_jf39@student.itstep.kz>
2022-12-05 21:42:08 +0600

    Добавьте файлы проекта.

---
+++ WPF/Helper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF
+{
+    internal class Helper
+    {
+        public static string SentMsg(string to, string subject, string body, out bool result)
+        {
+            MailMessage msg = new MailMessage();   
+            msg.From = new MailAddress("beks.dimash@gmail.com", "Dimash Bexultanov");
+
+            msg.To.Add(to);
+            //msg.CC.Add(""); //copy BCC - hidden copy
+
+            msg.Subject = subject;
+            msg.Priority = MailPriority.High;
+            msg.Body = body;
+            //msg.IsBodyHtml = true;  if body is html
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("login", "password");
+
+            try
+            {
+                smtp.Send(msg);
+                result = true;
+                return "Сообщение отправлено";
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                return ex.Message;
+            }
+        } 
+    }
+}
---
+++ WPF/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TestWin.Title = "TestWindow";
+
+            //btnShowAlert.Click += btnShowAlert_Click; // делегат
+
+            //Button btn = new Button();
+            //btn.Content = "Button";
+            //btn.Height = 50;
+            //btn.Width = 50;
+
+            //TestWin.Content = btn;
+        }
+
+        private void btnShowAlert_Click(object sender, RoutedEventArgs e)  //sender - с кем происходит  e - что происходит
+        {
+            Button btn = (Button)sender;
+
+            MessageBox.Show("Work - "+DateTime.Now);
+        }
+
+        private void btnSentMsg_Click(object sender, RoutedEventArgs e)
+        {
+            bool result = false;
+            lblMessage.Content = Helper.SentMsg(tbxTo.Text, tbxSubject.Text, tbxBody.Text, out result);
+
+            if (!result)
+            {
+                lblMessage.Foreground = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                lblMessage.Foreground = new SolidColorBrush(Colors.Green);
+            }
+        }
+    }
+}
